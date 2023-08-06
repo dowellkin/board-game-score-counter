5ab43ed5c8c55c228a0ea8842923998b6338383a
Alexandr Korolev <dowellkin@gmail.com>
2023-08-06 14:03:50 +0300

    Test Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
+import Header from "./components/Header/Header"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header></Header>
       <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-indigo-400 text-black font-semibold">
+      <div className="container max-w-5xl mx-auto">test</div>
+    </header>
+  )
+}
+
+export default Header
